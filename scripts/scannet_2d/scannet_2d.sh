dfa69e09b1c166eaa5d8dcbf7bc6e36be32a39c3
pushkalkatara <pkatara@andrew.cmu.edu>
2023-04-05 15:45:44 -0400

    minor

--- scripts/scannet_2d/scannet_2d.sh
+++ scripts/scannet_2d/scannet_2d.sh
@@ -16,13 +16,14 @@ general.train_on_segments=true
 
 # TEST
 # python main_instance_segmentation.py \
-# general.experiment_name="validation_query_${CURR_QUERY}_topk_${CURR_TOPK}_dbscan_${CURR_DBSCAN}" \
-# general.project_name="scannet_eval" \
-# general.checkpoint='checkpoints/scannet/scannet_val.ckpt' \
+# general.experiment_name="scannet_2d_validation_query_${CURR_QUERY}_topk_${CURR_TOPK}_dbscan_${CURR_DBSCAN}" \
+# general.project_name="scannet_2d_eval" \
+# general.checkpoint='/home/pkatara/Mask3D/saved/2d/last-epoch.ckpt' \
 # general.train_mode=false \
 # general.eval_on_segments=true \
 # general.train_on_segments=true \
 # model.num_queries=${CURR_QUERY} \
 # general.topk_per_image=${CURR_TOPK} \
 # general.use_dbscan=true \
-# general.dbscan_eps=${CURR_DBSCAN}
+# general.dbscan_eps=${CURR_DBSCAN} \
+# data/datasets=scannet_2d
